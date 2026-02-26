73db2f59a3d27eba1af844011ea83f6fcc64ac19
Derek Flores <derekflores992@gmail.com>
2026-02-25 18:31:27 -0600

    huespedresponse regresa nombre separado

--- common-reservaciones/src/main/java/com/vdr/common_reservaciones/dtos/huespedes/HuespedResponse.java
+++ common-reservaciones/src/main/java/com/vdr/common_reservaciones/dtos/huespedes/HuespedResponse.java
@@ -4,6 +4,8 @@ package com.vdr.common_reservaciones.dtos.huespedes;
 public record HuespedResponse(
 		Long id, 
 		String nombre, 
+		String apellidoMaterno,
+		String apellidoPaterno,
 		String email,
 		String telefono, 
 		String tipoDocumento,
